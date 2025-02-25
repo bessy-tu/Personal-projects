1c709b8e7d25c6c4e46e719efd85c3665295428d
bessy-tu <bessytd26@gmail.com>
2025-02-25 18:58:04 +0800

    0225

--- js/asteroid.js
+++ js/asteroid.js
@@ -1,44 +1,10 @@
-// $(document).ready(function () {
-//     let index = 0;  // 初始化圖片索引
-
-//     // 當點擊左右按鈕時切換圖片
-//     $(".left").click(function () {
-//         moveSlide(-1);
-//     });
-
-//     $(".right").click(function () {
-//         moveSlide(1);
-//     });
-
-//     // 切換圖片
-//     function moveSlide(direction) {
-//         const $images = $(".carousel-img");
-//         const totalImages = $(".carousel-img img").length;
-//         index += direction;
-
-//         // 圖片索引超出範圍時重置
-//         if (index >= totalImages) {
-//             index = 0;
-//         } else if (index < 0) {
-//             index = totalImages - 1;
-//         }
-
-//         // 移動圖片容器，顯示對應圖片
-//         $images.css("transform", `translateX(-${index * 650}px)`);
-
-//         // if (windowWidth <= 1536) {
-//         //     $images.css("transform", `translateX(-${index * 500}px)`);
-//         // }
-//     }
-// });
-
 $(document).ready(function () {
-    let index = 0;  // 初始化圖片索引
-    const $images = $(".carousel-img");  // 圖片容器
-    const $imageContainer = $(".carousel-img img");  // 單張圖片元素
-    const totalImages = $imageContainer.length;  // 總圖片數量
+    let index = 0; 
+    const $images = $(".carousel-img");  
+    const $imageContainer = $(".carousel-img img"); 
+    const totalImages = $imageContainer.length; 
 
-    // 當點擊左右按鈕時切換圖片
+    // 點擊左右按鈕時切換圖片
     $(".left").click(function () {
         moveSlide(-1);  
     });
@@ -52,9 +18,9 @@ $(document).ready(function () {
         const windowWidth = $(window).width();  // 獲取當前視窗寬度
         index += direction;  // 根據方向增減圖片索引
 
-        // 圖片索引超出範圍時重置
+        // 圖片超出範圍時重置
         if (index >= totalImages) {
-            index = 0;  // 如果超過最大索引，回到第一張
+            index = 0;  // 如果超過，回到第一張
         } else if (index < 0) {
             index = totalImages - 1;  // 如果小於 0，回到最後一張
         }
@@ -62,7 +28,7 @@ $(document).ready(function () {
         // 根據視窗寬度設置圖片容器的移動距離
         let imageWidth = 650;  // 預設圖片寬度
         if (windowWidth <= 1536) {
-            imageWidth = 500;  // 當視窗寬度小於等於 1536px 時，使用 500px 寬度
+            imageWidth = 500;  
         }
 
         // 移動圖片容器，顯示對應圖片
